f80b9115975d4b781d0cde09468ea07d2db54d5e
kirankumarn07 <kirankumar.naga7@gmail.com>
2024-04-10 17:36:50 +0530

    first commit of image gallery app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from 'react'
+import {Typography,AppBar,Toolbar,Container,ImageList,ImageListItem,TextField,center,Button} from '@mui/material'
+import CameraEnhanceIcon from '@material-ui/icons/CameraEnhance';
+import ImagesData from './ImagesData.json'
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <AppBar>
+        <Toolbar>
+        <CameraEnhanceIcon/>
+          <Typography variant='h4'>Photography</Typography>
+        </Toolbar>
+      </AppBar>
+     <Container style={{"marginTop":"70px","textAlign":"center"}}>
+       <Typography variant='h3'><b>Our Latest Photos</b></Typography>
+       <Typography paragraph style={{"color":"orange"}}>WHICH OF MY PHOTOGRAPHS IS MY FAVORITE? THE ONE  I AM GOING TO TAKE TOMORROW</Typography>
+     </Container>
+     <Container>
+      <ImageList rowHeight={170} cols={3}>
+               {ImagesData.map(imageObj=>
+                 <ImageListItem cols={1}>
+                  <img src={imageObj.userImageURL} alt={imageObj.user} style={{"padding":"5px","height":"50px","width":"250px",
+                }}/>
+                 </ImageListItem>
+               )}
+      </ImageList>
+     </Container>
+    
+     <center>
+     <Container>
+      <Typography variant='h4' style={{"color":"greenyellow"}}>Contact Form</Typography>
+      <form>
+          <TextField style={{"width":"500px","margin":"5px"}} type="text" placeholder='firstname' lable="firstname" variant="outlined"/><br/>
+          <TextField style={{"width":"500px","margin":"5px"}} type="text" placeholder='lastname'lable="lastName" variant="outlined"/><br />
+          <TextField style={{"width":"500px","margin":"5px"}} type="text" placeholder='email'lable="Email" variant="outlined"/><br />
+          <TextField style={{"width":"500px","margin":"5px"}} type="text" placeholder='mobileno'lable="MobileNo" variant="outlined"/><br />
+          <Button variant="contained" color="warning">Submit</Button>
+      </form>
+     </Container>
+     </center>
+     </> 
+  )
 }
 
-export default App;
+export default App
